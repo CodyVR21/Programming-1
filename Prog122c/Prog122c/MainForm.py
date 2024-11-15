c5fbe831843b77228c89f0240e406b47735c240f
CodyVR21 <153629372+CodyVR21@users.noreply.github.com>
2024-11-15 09:37:09 -0600

    hi

---
+++ Prog122c/Prog122c/MainForm.py
@@ -0,0 +1,94 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
+        self._listBox1 = System.Windows.Forms.ListBox()
+        self.SuspendLayout()
+        # 
+        # button1
+        # 
+        self._button1.BackColor = System.Drawing.Color.Maroon
+        self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        self._button1.Location = System.Drawing.Point(5, 394)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(124, 49)
+        self._button1.TabIndex = 0
+        self._button1.Text = "Calculate"
+        self._button1.UseVisualStyleBackColor = False
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.BackColor = System.Drawing.Color.Maroon
+        self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        self._button2.Location = System.Drawing.Point(211, 398)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(121, 49)
+        self._button2.TabIndex = 1
+        self._button2.Text = "Clear"
+        self._button2.UseVisualStyleBackColor = False
+        self._button2.Click += self.Button2Click
+        # 
+        # button3
+        # 
+        self._button3.BackColor = System.Drawing.Color.Maroon
+        self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button3.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        self._button3.Location = System.Drawing.Point(420, 398)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(110, 49)
+        self._button3.TabIndex = 2
+        self._button3.Text = "Exit"
+        self._button3.UseVisualStyleBackColor = False
+        self._button3.Click += self.Button3Click
+        # 
+        # listBox1
+        # 
+        self._listBox1.BackColor = System.Drawing.Color.DarkRed
+        self._listBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._listBox1.ForeColor = System.Drawing.Color.White
+        self._listBox1.FormattingEnabled = True
+        self._listBox1.ItemHeight = 31
+        self._listBox1.Location = System.Drawing.Point(5, 12)
+        self._listBox1.Name = "listBox1"
+        self._listBox1.Size = System.Drawing.Size(537, 376)
+        self._listBox1.TabIndex = 3
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.Color.Red
+        self.ClientSize = System.Drawing.Size(542, 473)
+        self.Controls.Add(self._listBox1)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Name = "MainForm"
+        self.Text = "Prog122c"
+        self.ResumeLayout(False)
+
+    def Button2Click(self, sender, e):
+        self._listBox1.Items.Clear()
+    
+    def Button3Click(self, sender, e):
+        Application.Exit()
+
+    def Button1Click(self, sender, e):
+        for num in range(1, 5+1):
+            one = num * 2
+            two = int(one) + 1
+            three = int(one) * 2
+            four = int(one)**2
+            line = str(one) + "\t" + str(two) + "\t" + str(three) + "\t" + str(four)
+            self._listBox1.Items.Add(line)
